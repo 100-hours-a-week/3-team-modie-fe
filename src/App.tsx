600e389f1b2592e30cd277c0d04e62485904d519
gmlwlsdl <heejin094@naver.com>
2025-03-12 13:22:35 +0900

    chore: husky 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+// App.tsx
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="flex display bg-black justify-center items-center">
-        <div className="text-amber-200">
-          hihileifjalfjlaejfajfljalfalfnlaneflafjslfjalejcaejfajfaje;lacj;efjnnclalsjefljslefjlsefjasnflanscesni;einfa;efl;sjelf;saejif;eijf;selfija;eifjas;lcna;sleinf;cknalc.zefmaf;a.lmc.
-        </div>
+        <div className="text-amber-200">hihi testtest</div>
       </div>
     </>
   );
