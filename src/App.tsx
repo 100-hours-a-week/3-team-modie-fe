72305b150f0a9267edda1a8b549d615dd44f8e20
gmlwlsdl <heejin094@naver.com>
2025-03-12 13:18:42 +0900

    chore: husky, lint-staged 적용, 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
       <div className="flex display bg-black justify-center items-center">
-        <div className="text-amber-200">hihi</div>
+        <div className="text-amber-200">
+          hihileifjalfjlaejfajfljalfalfnlaneflafjslfjalejcaejfajfaje;lacj;efjnnclalsjefljslefjlsefjasnflanscesni;einfa;efl;sjelf;saejif;eijf;selfija;eifjas;lcna;sleinf;cknalc.zefmaf;a.lmc.
+        </div>
       </div>
     </>
   );
