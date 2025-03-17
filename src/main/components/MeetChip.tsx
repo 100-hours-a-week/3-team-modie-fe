f9d51e03e864b56f3c1cc532d7829cd55728e41c
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-18 01:07:37 +0900

    feat: chip 구현

---
+++ src/main/components/MeetChip.tsx
@@ -0,0 +1,28 @@
+interface MeetChipProps {
+  title: string;
+  isSelected?: boolean;
+  onClick?: () => void;
+}
+
+export default function MeetChip({
+  title,
+  isSelected = false,
+  onClick,
+}: MeetChipProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`
+                flex items-center justify-center gap-2.5 rounded-full w-fit h-fit self-stretch px-[1.6rem] py-[0.8rem]
+                text-center text-Body2 align-middle
+                ${
+                  isSelected
+                    ? `bg-primaryDark2 font-bold text-white`
+                    : `bg-white border-1 border-gray75 font-pretendard text-gray75`
+                }
+            `}
+    >
+      {title}
+    </button>
+  );
+}
