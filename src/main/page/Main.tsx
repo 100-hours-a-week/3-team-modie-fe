12167962ebdd9ad4e31bd29c4cdc34b78485907b
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-18 01:08:17 +0900

    feat: 게시글 목록 페이지 구현

--- src/main/page/Main.tsx
+++ src/main/page/Main.tsx
@@ -1,21 +1,70 @@
 // import TestUserComponent from "../components/TestUserComponent";
 import Header from "../../common/components/Header";
-import ToastMsg from "../../common/components/ToastMsg";
-import SubmitBtn from "../../common/components/SubmitBtn";
+import MeetCard from "../components/MeetCard.tsx";
+import MeetTap from "../components/MeetTab.tsx";
+import MeetChip from "../components/MeetChip.tsx";
+import { useEffect, useState } from "react";
+// import {useQuery} from "@tanstack/react-query";
 
 export default function Main() {
+  const [activeTab, setActiveTab] = useState("참여중");
+  const [selectedChip, setSelectedChip] = useState("전체");
+  const chipCategories = ["전체", "음식", "운동", "이동", "기타"];
+
+  useEffect(() => {
+    console.log(`필터링 : ${activeTab} / ${selectedChip}`);
+  }, [activeTab, selectedChip]);
+
+  const handleChipClick = (category: string) => {
+    setSelectedChip(category);
+  };
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       {/* tanstack-query 테스트용도
       <TestUserComponent /> */}
-      <Header isMainPage />
+      <div className="sticky top-0 z-10 bg-white">
+        <Header isMainPage />
 
-      <div className="fixed bottom-29 w-full flex justify-center ">
-        <ToastMsg active={true} description="토스트 메세지 컴포넌트" />
+        {/* 탭 */}
+        <div className="flex w-full h-fit justify-between">
+          <MeetTap
+            title="참여중"
+            isSelected={activeTab === "참여중"}
+            onClick={() => handleTabClick("참여중")}
+          />
+          <MeetTap
+            title="종료"
+            isSelected={activeTab === "종료"}
+            onClick={() => handleTabClick("종료")}
+          />
+        </div>
+
+        {/* 칩 */}
+        <div className="flex px-[2rem] py-[1.2rem] gap-4 w-full h-fit justify-between">
+          {chipCategories.map((category) => (
+            <MeetChip
+              key={category}
+              title={category}
+              isSelected={selectedChip === category}
+              onClick={() => handleChipClick(category)}
+            />
+          ))}
+        </div>
       </div>
-
-      <div className="fixed bottom-0 w-full flex justify-center pb-6">
-        <SubmitBtn active={true} description="다음" />
+      <div className="flex flex-col px-[2rem] py-[1rem] gap-[2rem]">
+        <MeetCard meetKey={1} />
+        <MeetCard meetKey={2} />
+        <MeetCard meetKey={3} />
+        <MeetCard meetKey={3} />
+        <MeetCard meetKey={3} />
+        <MeetCard meetKey={3} />
+        <MeetCard meetKey={3} />
+        <MeetCard meetKey={3} />
       </div>
     </div>
   );
