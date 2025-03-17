576e1e74be18b253e7ee303ef23982a41300142c
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-17 23:25:21 +0900

    fix: img 태그 alt 추가

--- src/main/components/MeetCard.tsx
+++ src/main/components/MeetCard.tsx
@@ -24,14 +24,22 @@ export default function MeetCard() {
       </div>
 
       <div className="flex h-fit items-center gap-2 self-stretch">
-        <img src={locationIcon} className="w-[2.4rem] h-[2.4rem]" />
+        <img
+          src={locationIcon}
+          className="w-[2.4rem] h-[2.4rem]"
+          alt="위치 아이콘"
+        />
         <div className="text-gray75 text-Body3 font-pretendard truncate w-full text--">
           문 앞 (제주 제주시 월성로 4길)
         </div>
       </div>
 
       <div className="flex h-fit items-center gap-2 self-stretch">
-        <img src={clockIcon} className="w-[2.4rem] h-[2.4rem]" />
+        <img
+          src={clockIcon}
+          className="w-[2.4rem] h-[2.4rem]"
+          alt="시간아이콘"
+        />
         <div className="text-gray75 text-Body3 font-pretendard truncate w-full text--">
           23.07.07 (일) 13:00
         </div>
@@ -41,6 +49,7 @@ export default function MeetCard() {
         <img
           src={memeberIcon}
           className="w-[2.4rem] h-[2.4rem] px-0.5 py-0.5"
+          alt="맴버 아이콘"
         />
         <div className="text-gray75 text-Body3 font-pretendard truncate w-full text--">
           2/3 명
