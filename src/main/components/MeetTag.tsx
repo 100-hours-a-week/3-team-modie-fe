12c82bd04f7cf71947900c641c62600e7e6cb3fb
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-18 23:55:58 +0900

    fix: MeetTag border 1 -> 0.5

--- src/main/components/MeetTag.tsx
+++ src/main/components/MeetTag.tsx
@@ -4,7 +4,7 @@ interface MeetTagProps {
 
 export default function MeetTag({ name }: MeetTagProps) {
   return (
-    <div className="flex items-center justify-center gap-2 px-2 py-0.5 rounded border border-primaryDark3">
+    <div className="flex items-center justify-center gap-2 px-2 py-0.5 rounded border-[0.5px] border-primaryDark3">
       <div className="text-primaryDark3 font-pretendard text-Caption2 leading-[1.4rem]">
         {name || "태그명"}
       </div>
