30e26ab945f374bcfb087249e9c352ebe1e66fe9
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-19 02:07:48 +0900

    fix: sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -87,7 +87,7 @@ define(["./workbox-54d0af47"], function (workbox) {
       },
       {
         url: "index.html",
-        revision: "0.9075cn4gf98",
+        revision: "0.6288ld7l4do",
       },
     ],
     {}
