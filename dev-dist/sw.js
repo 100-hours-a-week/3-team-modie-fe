9e6c2e99ddb642b87e1d22dadb2893e1af40bfa8
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-21 02:13:50 +0900

    fix: title 추가 및 정산 금액 검증 추가 - title : 모임 정산 추가 - 정산 금액 : 1000만원 이하로 입력 가능하도록 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -87,7 +87,7 @@ define(["./workbox-54d0af47"], function (workbox) {
       },
       {
         url: "index.html",
-        revision: "0.gphtag2da9g",
+        revision: "0.ij4vnfk7s0o",
       },
     ],
     {}
