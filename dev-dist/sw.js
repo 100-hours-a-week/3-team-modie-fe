1a0b2d033b2a66029ca58fae26d2d4d6ac17ef91
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-24 17:15:16 +0900

    chore: 변경사항 확인을 위한 commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -87,7 +87,7 @@ define(["./workbox-54d0af47"], function (workbox) {
       },
       {
         url: "index.html",
-        revision: "0.isv12i5rt6o",
+        revision: "0.n4t95lopcqo",
       },
     ],
     {}
