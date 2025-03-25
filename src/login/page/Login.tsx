f2f13fe0cb384e1257dcb9a8d6c4c15a3a707015
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-25 17:52:58 +0900

    fix: 환경변수명 변경

--- src/login/page/Login.tsx
+++ src/login/page/Login.tsx
@@ -4,7 +4,7 @@ import kakaoLogin from "../../assets/kakao_login_large_wide.png";
 const key = import.meta.env.VITE_KAKAO_REST_API_KEY;
 const redirectUrl = import.meta.env.VITE_KAKAO_REDIRECT_URI_DEV;
 
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${key}redirect_uri=${redirectUrl}&response_type=code`;
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${key}&redirect_uri=${redirectUrl}&response_type=code`;
 
 export default function Login() {
   const handleLogin = () => {
