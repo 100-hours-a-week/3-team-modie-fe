9c19eb10750a1abb46fc9fe7ca8cb84c345d4f65
gmlwlsdl <heejin094@naver.com>
2025-03-27 16:48:49 +0900

    feat: 메타 데이터 고정 및 helmet 제거

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,11 @@ import "./global.css";
 import Routers from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-calendar/dist/Calendar.css"; // 기본 스타일 불러오되 일부만 덮어씀
-import { HelmetProvider } from "react-helmet-async";
-import AppMeta from "./AppMeta";
 
 const queryClient = new QueryClient(); // tanstack-query용 설정
-const helmetContext = {};
 
 createRoot(document.getElementById("root")!).render(
-  <HelmetProvider context={helmetContext}>
-    <QueryClientProvider client={queryClient}>
-      <AppMeta />
-      <Routers />
-    </QueryClientProvider>
-  </HelmetProvider>
+  <QueryClientProvider client={queryClient}>
+    <Routers />
+  </QueryClientProvider>
 );
