1615dc5095c2ba5e2df9254619381fefc5d73ca8
gmlwlsdl <heejin094@naver.com>
2025-03-27 15:01:43 +0900

    feat: 링크 공유시 모임 미리보기 기능 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import Routers from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-calendar/dist/Calendar.css"; // 기본 스타일 불러오되 일부만 덮어씀
+import { HelmetProvider } from "react-helmet-async";
 
 const queryClient = new QueryClient(); // tanstack-query용 설정
+const helmetContext = {};
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <HelmetProvider context={helmetContext}>
     <QueryClientProvider client={queryClient}>
       <Routers />
     </QueryClientProvider>
-  </StrictMode>
+  </HelmetProvider>
 );
