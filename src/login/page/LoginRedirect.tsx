2d26fa02916e469365a15d20cfa63ebfca30f07a
gmlwlsdl <heejin094@naver.com>
2025-03-27 15:48:36 +0900

    chore: VITE_LOGIN_URI 환경변수 추가

--- src/login/page/LoginRedirect.tsx
+++ src/login/page/LoginRedirect.tsx
@@ -6,13 +6,13 @@ import Splash from "../../common/page/Splash";
 export default function KakaoCallback() {
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
-  const redirect_uri = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+  const login_url = import.meta.env.VITE_LOGIN_URI;
   const navigate = useNavigate();
 
   useEffect(() => {
     if (code) {
       axios
-        .get(`${redirect_uri}?code=${code}`) // ✅ GET 요청으로 변경
+        .get(`${login_url}?code=${code}`) // ✅ GET 요청으로 변경
         // .get(`http://localhost:8080/auth/kakao/login?code=${code}`) // ✅ GET 요청으로 변경
         .then((res) => {
           console.log("로그인 성공", res.data);
