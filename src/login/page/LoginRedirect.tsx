f0f0f4ef5aed4c2c4bb95019907c1891db85376b
gmlwlsdl <heejin094@naver.com>
2025-03-27 14:20:41 +0900

    chore: 카카오 로그인 redirect url 환경변수로 관리

--- src/login/page/LoginRedirect.tsx
+++ src/login/page/LoginRedirect.tsx
@@ -6,17 +6,22 @@ import Splash from "../../common/page/Splash";
 export default function KakaoCallback() {
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
+  const redirect_uri = import.meta.env.VITE_KAKAO_REDIRECT_URI;
   const navigate = useNavigate();
 
   useEffect(() => {
     if (code) {
       axios
-        .get(`https://dev-api.modie.site/auth/kakao/login?code=${code}`) // ✅ GET 요청으로 변경
+        .get(`${redirect_uri}?code=${code}`) // ✅ GET 요청으로 변경
         // .get(`http://localhost:8080/auth/kakao/login?code=${code}`) // ✅ GET 요청으로 변경
         .then((res) => {
           console.log("로그인 성공", res.data);
           localStorage.setItem("accessToken", res.data.data);
-          navigate("/"); // 로그인 후 메인 페이지로 이동
+
+          const redirectPath =
+            localStorage.getItem("afterLoginRedirect") || "/";
+          localStorage.removeItem("afterLoginRedirect");
+          navigate(redirectPath);
         })
         .catch((err) => console.error("로그인 실패", err));
     }
