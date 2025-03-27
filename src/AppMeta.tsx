24d49877251493751e1b702e354a2c3e7a39a8d6
gmlwlsdl <heejin094@naver.com>
2025-03-27 16:42:13 +0900

    feat: 메타 데이터 고정

--- src/AppMeta.tsx
+++ src/AppMeta.tsx
@@ -4,6 +4,19 @@ export default function AppMeta() {
   return (
     <Helmet>
       <meta charSet="UTF-8" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="MODiE" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:image" content="https://i.ibb.co/v4W38BxK/image.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      <meta property="og:description" content="모임을 더 쉽고 편하게!" />
+      <meta property="og:locale" content="ko_KR" />
+
+      <meta name="twitter:title" content="MODiE" />
+      <meta name="twitter:description" content="모임을 더 쉽고 편하게!" />
+      <meta property="og:image" content="https://i.ibb.co/v4W38BxK/image.png" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, minimum-scale=1, viewport-fit=cover"
