50c878472da4b510487efc7cc5c2359ab63e84d0
gmlwlsdl <heejin094@naver.com>
2025-03-31 16:52:39 +0900

    fix: init fcm 위치 변경

--- src/login/page/LoginRedirect.tsx
+++ src/login/page/LoginRedirect.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Splash from "../../common/page/Splash";
-import { initFCM } from "../../utils/fcm";
 
 export default function KakaoCallback() {
   const [searchParams] = useSearchParams();
@@ -16,8 +15,6 @@ export default function KakaoCallback() {
         .get(`${login_url}?code=${code}`)
         .then((res) => {
           localStorage.setItem("accessToken", res.data.data);
-
-          initFCM();
 
           const redirectPath =
             localStorage.getItem("afterLoginRedirect") || "/";
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import "./global.css";
 import Routers from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-calendar/dist/Calendar.css";
+import { initFCM } from "./utils/fcm";
 
 // React Query 클라이언트
 const queryClient = new QueryClient();
+initFCM();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
